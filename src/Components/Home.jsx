91b29d883dceb370d6ecc7aec6431451e3743288
soloTurksFalcons <muhammad.arshad@ist.edu.pk>
2025-04-02 02:46:50 +0000

    package.json modified

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/MuhammadAmir.jpg";
+import image from "../images/MuhammadAmir.jpeg";
 // import image from "../images/MuhammadAmir.jpg"; // Replace with your image path
 
 const imageAltText = "<Muhammad Amir>";
